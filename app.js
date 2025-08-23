a7b51d76726eadd7aa953d9a3c1f5aecb2d09e68
anamol04 <sh.anmol23@ncmt.edu.np>
2025-08-23 14:06:31 +0545

    Setting Up Things

---
+++ app.js
@@ -0,0 +1,15 @@
+const express= require("express");
+const app = express();
+const port= 8080;
+const mongoose= require("mongoose");
+const MONGODB_URL= "mongodb://127.0.0.1:270178/wanderlust";
+
+
+main().then(()=>console.log("MongoDB Successfully Connected...")).catch(error=>console.log(error));
+async function main(){
+    await mongoose.connect(MONGODB_URL);
+}
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port number ${port}..`);
+})
