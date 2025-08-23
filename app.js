85906ef60462c244c8461ef944988e2732685c24
anamol04 <sh.anmol23@ncmt.edu.np>
2025-08-23 14:23:52 +0545

    Few more increment.

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express();
 const port= 8080;
 const mongoose= require("mongoose");
 const MONGODB_URL= "mongodb://127.0.0.1:270178/wanderlust";
+const Listing= require("./models/listing.js")
+
+
+app.set("view engine" , "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
 
 
 main().then(()=>console.log("MongoDB Successfully Connected...")).catch(error=>console.log(error));
